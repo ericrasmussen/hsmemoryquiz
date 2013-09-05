5630ad8811c7f0f9c6026f57e7a51b61b556517e
Eric Rasmussen <ericrasmussen@gmail.com>
2013-09-05 00:20:41 -0700

    clean up letter tests

--- test/LetterSpec.hs
+++ test/LetterSpec.hs
@@ -10,13 +10,28 @@ import Digit
 import Letter
 
 
+-- Letters start at A (corresponding to One) and end at O (corresponding to
+-- Zero). We don't have Letter -> Digit functions to test with because they
+-- aren't needed in the application, but this is convenient for the tests.
+digits :: [Digit]
+digits = [One .. Nine] ++ [Zero]
+
+
 spec :: Spec
 spec = do
-  describe "Letters" $
+  describe "Letters" $ do
     it "can be created from a digit" $ do
-      let mapping = zip [A .. O] ([One .. Nine] ++ [Zero])
+      let mapping = zip [A .. O] digits
       property $ \d -> case lookup (fromDigit d) mapping of
         Just digit -> d == digit
         Nothing    -> False
 
+    it "correspond to the list of digits" $
+      [A .. O] `shouldBe` map fromDigit digits
+
+  describe "LetterPairs" $
+    it "can be created from a DigitPair" $
+      property $ \ds -> let (DigitPair  n m) = ds
+                            (LetterPair c d) = digitsToLetters ds
+                        in fromDigit n == c && fromDigit m == d
 
